5727cdec6bde8357428af81a3206917db803e471
Emmsfay <emmychukwudi215@gmail.com>
2026-03-16 23:32:27 +0100

    fix: update vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => {
       host: "0.0.0.0",
       port: 5173,
       proxy: {
-        "/api": {
+        "/api": { 
           target: env.VITE_API_URL || "http://backend:8000",
           changeOrigin: true,
         },
